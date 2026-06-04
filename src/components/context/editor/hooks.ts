52ee707d6e6defadaf5a6860943885cf5754f36e
현빈 <hhb041127@naver.com>
2026-06-04 10:53:48 +0900

    fix: getSnapshot의 참조가 매 렌더마다 새로 생성되는 이슈 수정

--- src/components/context/editor/hooks.ts
+++ src/components/context/editor/hooks.ts
@@ -1,24 +1,26 @@
-import { useContext, useSyncExternalStore } from "react";
+import { useCallback, useContext, useSyncExternalStore } from "react";
 import { MarkdownEditorContext } from "./contexts";
 
 export const useEditor = () => {
   const editor = useContext(MarkdownEditorContext);
-
   if (editor == null) {
     throw new Error("useEditor must be used within a MarkdownEditorProvider");
   }
-
   return editor;
 };
 
 export const useBlock = (id: string) => {
   const editor = useEditor();
 
-  return useSyncExternalStore(editor.subscribe, () => editor.state.nodes[id]);
+  const getSnapshot = useCallback(() => editor.state.nodes[id], [editor, id]);
+
+  return useSyncExternalStore(editor.subscribe, getSnapshot);
 };
 
 export const useRootIds = () => {
   const editor = useEditor();
 
-  return useSyncExternalStore(editor.subscribe, () => editor.state.rootIds);
+  const getSnapshot = useCallback(() => editor.state.rootIds, [editor]);
+
+  return useSyncExternalStore(editor.subscribe, getSnapshot);
 };
